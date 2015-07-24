b320cf547d5fe1028f11cf0e1b74b1c197e0d4d7
Daan Mathot <daanmathot@gmail.com>
2015-07-25 00:55:32 +0200

    clean up

--- stash.js
+++ stash.js
@@ -6,16 +6,20 @@
 // @author       Daan Mathot <daanmathot@gmail.com>
 // @match        http://stash.infostrada.com/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
-
+// @grant        GM_addStyle
 // @grant        GM_setClipboard
 // ==/UserScript==
 
 
 var enabledUtils = [injectCss, addCopyBranchNameToClipboardButton];
 
+// init utils
 enabledUtils.forEach(function(util) {
     util();
 });
+
+
+/** SHARED **/
 
 function notify(msg) {
     var $notification = $('<span/>', {'class': 'notification', text: msg});
@@ -35,14 +39,17 @@ function injectCss() {
     var css  = '.notification { display: block; position: absolute; width: 24%; background: #eee; z-index: 9999; top: 5%;' +
         'left: 38%; border: 1px solid #ccc; text-align: center; color: #4c4c4c; padding: 0.5% 0;  font-size: 115%; font-weight: bold; }';
 
-    $('body').prepend($('<style/>', {text: css}));
+    GM_addStyle(css);
 }
+
+
+/** UTILS **/
 
 function addCopyBranchNameToClipboardButton() {
     var $button = $('<button/>', {
         text: 'Copy branch name to clipboard',
         'class': 'aui-button'
-    });
+    })
     $button.click(function() {
         var branchName = document.querySelector('span[data-id]').getAttribute('title');
         GM_setClipboard(branchName, false);
@@ -52,3 +59,4 @@ function addCopyBranchNameToClipboardButton() {
 
     $('.find-files').append($button);
 }
+
